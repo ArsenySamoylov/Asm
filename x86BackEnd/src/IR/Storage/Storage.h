91083bb3aadcf1d30ab52f7b0c8277727e9e1359
ArsenyFucker <FuckYou@very.hard>
2023-06-06 00:38:36 +0300

    Writing more documentation

--- x86BackEnd/src/IR/Storage/Storage.h
+++ x86BackEnd/src/IR/Storage/Storage.h
@@ -1,3 +1,7 @@
+/*!
+\file
+\brief This file contains declaration of Storage class 
+*/
 #pragma once
 
 #include "NoCopyable.h"
@@ -25,7 +29,10 @@ enum class VariableType
     NotVariable,
     };
 
-// TODO rename storage_data to storage_info
+/**
+ * @brief Structure to contain data and information about Storage. 
+ * @todo rename storage_data to storage_info
+ */
 struct StorageData
     {
     address_t        address; // if it is GlobalVar, BaseBlock or Function
@@ -34,16 +41,22 @@ struct StorageData
     size_t offset;            // if in stack
     };
 
+/**
+ * @brief Storage objects shows state of Value.
+ * 
+ * Storage objects shows state of Value. For example result after executing Instruction
+ * can be placed in register or on stack. 
+ */
 class Storage : public NoCopyable 
     {
     private:
         enum   StorageType  storage_type;
         enum   VariableType variable_type;
-        struct StorageData  storage_data;      // storage data interpretation depends on StorageType 
+        struct StorageData  storage_data; // storage data interpretation depends on StorageType 
         
         size_t n_usage;
 
-        const name_t owner_name; // for debug 
+        const name_t owner_name; // for debug purposes
     
     public:
          Storage (StorageType type, VariableType var_type, name_t owner_name = NULL);
@@ -56,8 +69,17 @@ class Storage : public NoCopyable
         VariableType get_var_type () const;
 
         StorageData* set_storage_data ();
-         
+
+        /**
+         * @brief Increase number of usage
+         * 
+         */
         void increase_usage ();
+
+        /**
+         * @brief Decrease usage. If storage is no more used, than it frees allocated resources (register)
+         * 
+         */
         void decrease_usage ();
         size_t get_usage () const;
         
@@ -66,7 +88,17 @@ class Storage : public NoCopyable
         size_t            get_stack_offset () const;
         address_t         get_address      () const;
         
+        /**
+         * @brief Set storage, to state that object in register.
+         * Set storage, to state that object in register. Sets register status to BUSY
+         * @param reg 
+         */
         void set_with_reg (Reg* reg);
+
+        /**
+         * @brief Print storage to stdin (debug purposes)
+         * 
+         */
         void print () const;
 
         void move_to_reg (Context* ctx, Reg* dest_reg, const char* comment = NULL);
